52f2a7dd71d0373587e16623ec7186fad3febbf7
# <#>
2018-09-10 00:17:47 +0700

    update username to send msg

--- src/main/java/fr/delthas/skype/TestConnect.java
+++ src/main/java/fr/delthas/skype/TestConnect.java
@@ -1,13 +1,11 @@
 package fr.delthas.skype;
 
-import java.nio.file.Paths;
-
 @SuppressWarnings({"javadoc", "static-method"})
 public class TestConnect {
 
-    public static final String USERNAME = "xxx@outlook.com";
-    public static final String PASSWORD = "...";
-    public static final String SEND_TO_ACCOUNT = "...";
+    public static final String USERNAME = "ductritran@gmail.com";
+    public static final String PASSWORD = "";
+    public static final String SEND_TO_ACCOUNT = "";
 
     // @Test (Disable it until a special test account is created)
     public void testConnect() {
@@ -17,22 +15,22 @@ public class TestConnect {
             skype.connect();
 
             for (User user : skype.getContacts()) {
-                System.out.println("user: " + user);
-                if (user.getUsername().equalsIgnoreCase(SEND_TO_ACCOUNT)) {
+                System.out.println(user);
+                if (user.getUsername().equalsIgnoreCase(transformUsername(SEND_TO_ACCOUNT))) {
                     user.sendMessage("Hi, " + user.getDisplayName() + ", what's up?");
+                    System.out.println("Sent to: " + user.getUsername() + " successfully.");
                 }
             }
 
-            for (Group group : skype.getGroups()) {
-                Thread.sleep(2000);
-                System.out.println("group: " + group.getTopic());
+//             for (Group group : skype.getGroups()) {
+//                 Thread.sleep(2000);
+//                 System.out.println("group: " + group.getTopic());
 
-                if (group.isSelfAdmin()) {
-                    System.out.println("isSelfAdmin: " + group.getTopic());
-//                    skype.sendGroupMessage(group, "Hi everyone. Do not reply this msg.");
-                }
-            }
-
+//                 if (group.isSelfAdmin()) {
+//                     System.out.println("isSelfAdmin: " + group.getTopic());
+// //                    skype.sendGroupMessage(group, "Hi everyone. Do not reply this msg.");
+//                 }
+//             }
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -46,6 +44,16 @@ public class TestConnect {
         } catch (InterruptedException e1) {
         }
         skype.disconnect();
+    }
+
+    private String transformUsername(String sendToAccount) {
+        if ( sendToAccount.indexOf("@") > 0 ){
+            sendToAccount = sendToAccount.substring(0, sendToAccount.indexOf("@"));
+        }
+
+        sendToAccount = "live:" + sendToAccount;
+
+        return sendToAccount;
     }
 
     public static void main(String... strings) {
